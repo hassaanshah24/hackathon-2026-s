39a8d3afc0f8b0de76b0d9dc820a56ed1145a316
arfasohailnexterse-commits <arfasohail.nexterse@gmail.com>
2026-05-21 12:06:43 +0500

    deployment 2.0

--- ai_seekho_backend/config/settings.py
+++ ai_seekho_backend/config/settings.py
@@ -8,13 +8,11 @@ BACKEND_DIR = Path(__file__).resolve().parent.parent
 load_dotenv(BACKEND_DIR / ".env")
 
 class Settings(BaseSettings):
-    GEMINI_API_KEY: str
-    MAPS_API_KEY: str
-    FIREBASE_CREDENTIALS_PATH: str
+    GEMINI_API_KEY: str = ""
+    MAPS_API_KEY: str = ""
+    FIREBASE_CREDENTIALS_PATH: str = "../ai-seekho--01-firebase-adminsdk-fbsvc-ff54490668.json"
     port: int = 8000
     host: str = "0.0.0.0"
-    ENV: str = "development"
-    CORS_ORIGINS: str = "http://localhost:*"
 
     model_config = SettingsConfigDict(
         env_file=str(BACKEND_DIR / ".env"),
